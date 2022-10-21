bdf1a3ef413000cce98a5bfdff147f92ee7048a2
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2022-10-21 13:57:05 +0200

    Using collapsed sections to hide advanced information

--- plugin/admin/class-plebeian-market-admin-screen-information.php
+++ plugin/admin/class-plebeian-market-admin-screen-information.php
@@ -62,53 +62,60 @@ class Plebeian_Market_Admin_Screen_Information
 			directly copy the shortcode for a specific item.
 		</p>
 		<p></p>
-		<p>
-			You can also pass parameters to configure how your widget will look like:
-		<ul>
-			<b>size</b> - You can pass <code>small</code>, <code>medium</code>, <code>big</code>, <code>huge</code> or directly putting the <code>%</code> of the page
-			that you want the widget to occupy. If no size specified, 30% will be used by default.
-			<br>
-			<code>[plebeian_show_buynow key="EZ" size="medium"]</code> <br>
-			or <br>
-			<code>[plebeian_show_buynow key="EZ" size="70"]</code>
-		</ul>
-		<ul>
-			<b>slideshow</b> - If you pass this parameter with the <code>false</code> value, there will not be a picture
-			slideshow and just the first image of the product will be shown.
-			<br>
-			<code>[plebeian_show_buynow key="EZ" size="small" slideshow="false"]</code>
-		</ul>
-		<ul>
-			<b>slideshow_delay</b> - You can pass this parameter to set the time to wait between slideshow transitions
-			in milliseconds. Default value is 4000 (4 seconds).
-			<br>
-			<code>[plebeian_show_buynow key="EZ" size="small" slideshow_delay="6000"]</code>
-		</ul>
-		<ul>
-			<b>show_price_fiat</b> - If you pass this parameter with the <code>false</code> value, the price of the product
-			in fiat will not be shown.
-			<br>
-			<code>[plebeian_show_buynow key="EZ" size="small" show_price_fiat="false"]</code>
-		</ul>
-		<ul>
-			<b>show_price_sats</b> - If you pass this parameter with the <code>false</code> value, the price of the product
-			in satoshis will not be shown.
-			<br>
-			<code>[plebeian_show_buynow key="EZ" size="small" show_price_sats="false"]</code>
-		</ul>
-		<ul>
-			<b>show_shipping_info</b> - If you pass this parameter with the <code>false</code> value, the shipping information of
-			the product will not be shown in the widget.
-			<br>
-			<code>[plebeian_show_buynow key="EZ" size="small" show_shipping_info="false"]</code>
-		</ul>
-		<ul>
-			<b>show_quantity_info</b> - If you pass this parameter with the <code>true</code> value, the information about
-			the quantity of products that you're selling will be shown in the widget. It's hidden by default.
-			<br>
-			<code>[plebeian_show_buynow key="EZ" size="small" show_quantity_info="true"]</code>
-		</ul>
-		</p>
+		> <a class="" data-bs-toggle="collapse" href="#buyNowSpecificWidgetAdvanced" role="button" aria-expanded="false" aria-controls="buyNowSpecificWidgetAdvanced">
+			See advanced options
+		</a>
+		<div class="collapse" id="buyNowSpecificWidgetAdvanced">
+			<div class="card card-body">
+				<p>
+					You can also pass parameters to configure how your widget will look like:
+				<ul>
+					<b>size</b> - You can pass <code>small</code>, <code>medium</code>, <code>big</code>, <code>huge</code> or directly putting the <code>%</code> of the page
+					that you want the widget to occupy. If no size specified, 30% will be used by default.
+					<br>
+					<code>[plebeian_show_buynow key="EZ" size="medium"]</code> <br>
+					or <br>
+					<code>[plebeian_show_buynow key="EZ" size="70"]</code>
+				</ul>
+				<ul>
+					<b>slideshow</b> - If you pass this parameter with the <code>false</code> value, there will not be a picture
+					slideshow and just the first image of the product will be shown.
+					<br>
+					<code>[plebeian_show_buynow key="EZ" size="small" slideshow="false"]</code>
+				</ul>
+				<ul>
+					<b>slideshow_delay</b> - You can pass this parameter to set the time to wait between slideshow transitions
+					in milliseconds. Default value is 4000 (4 seconds).
+					<br>
+					<code>[plebeian_show_buynow key="EZ" size="small" slideshow_delay="6000"]</code>
+				</ul>
+				<ul>
+					<b>show_price_fiat</b> - If you pass this parameter with the <code>false</code> value, the price of the product
+					in fiat will not be shown.
+					<br>
+					<code>[plebeian_show_buynow key="EZ" size="small" show_price_fiat="false"]</code>
+				</ul>
+				<ul>
+					<b>show_price_sats</b> - If you pass this parameter with the <code>false</code> value, the price of the product
+					in satoshis will not be shown.
+					<br>
+					<code>[plebeian_show_buynow key="EZ" size="small" show_price_sats="false"]</code>
+				</ul>
+				<ul>
+					<b>show_shipping_info</b> - If you pass this parameter with the <code>false</code> value, the shipping information of
+					the product will not be shown in the widget.
+					<br>
+					<code>[plebeian_show_buynow key="EZ" size="small" show_shipping_info="false"]</code>
+				</ul>
+				<ul>
+					<b>show_quantity_info</b> - If you pass this parameter with the <code>true</code> value, the information about
+					the quantity of products that you're selling will be shown in the widget. It's hidden by default.
+					<br>
+					<code>[plebeian_show_buynow key="EZ" size="small" show_quantity_info="true"]</code>
+				</ul>
+				</p>
+			</div>
+		</div>
 
 		<p></p>
 
@@ -116,23 +123,30 @@ class Plebeian_Market_Admin_Screen_Information
 		<p>Use this shortcode to show the full listing of your <i>Buy Now</i> products:</p>
 		<code>[plebeian_show_buynow_listing]</code>
 		<p></p>
-		<p>
-			You can use that shortcode directly to use the default parameters, which are:
-		<ul><b>size</b> - 15</ul>
-		<ul><b>slideshow</b> - false</ul>
-		<ul><b>slideshow_delay</b> - 4000</ul>
-		<ul><b>show_price_fiat</b> - true</ul>
-		<ul><b>show_price_sats</b> - true</ul>
-		<ul><b>show_shipping_info</b> - true</ul>
-		<ul><b>show_quantity_info</b> - false</ul>
+		> <a class="" data-bs-toggle="collapse" href="#buyNowListingWidgetAdvanced" role="button" aria-expanded="false" aria-controls="buyNowListingWidgetAdvanced">
+			See advanced options
+		</a>
+		<div class="collapse" id="buyNowListingWidgetAdvanced">
+			<div class="card card-body">
+				<p>
+					You can use that shortcode directly to use the default parameters, which are:
+				<ul><b>size</b> - 15</ul>
+				<ul><b>slideshow</b> - false</ul>
+				<ul><b>slideshow_delay</b> - 4000</ul>
+				<ul><b>show_price_fiat</b> - true</ul>
+				<ul><b>show_price_sats</b> - true</ul>
+				<ul><b>show_shipping_info</b> - true</ul>
+				<ul><b>show_quantity_info</b> - false</ul>
 
-		But you can also pass all the parameters available to be used for single products in the shortcode to change the default behavior.
-		The specified parameters will be used for all the products in the listing. So for example:
-		<br>
-		<code>[plebeian_show_buynow_listing slideshow="true" slideshow_delay="7000" size="10" show_price_fiat="false" show_shipping_info="false"]</code>
-		<br>
-		<p>This will show all the <i>Buy Now</i> products in the store, enable the picture slideshow, set the picture change delay to 7 seconds,
-			show the pictures at 10% the size of the screen, and hide the fiat price and the shipping info.</p>
+				But you can also pass all the parameters available to be used for single products in the shortcode to change the default behavior.
+				The specified parameters will be used for all the products in the listing. So for example:
+				<br>
+				<code>[plebeian_show_buynow_listing slideshow="true" slideshow_delay="7000" size="10" show_price_fiat="false" show_shipping_info="false"]</code>
+				<br>
+				<p>This will show all the <i>Buy Now</i> products in the store, enable the picture slideshow, set the picture change delay to 7 seconds,
+					show the pictures at 10% the size of the screen, and hide the fiat price and the shipping info.</p>
+			</div>
+		</div>
 
 		<p></p><br>
 
