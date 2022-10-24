7249d4c2dfdb6306b862447fe47ccf1739434551
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2022-10-24 17:15:55 +0200

    Slideshow js separated from the public one

---
+++ plugin/public/js/plebeian-market-public-slideshow.js
@@ -0,0 +1,66 @@
+function startSlideShows(slideshow) {
+    var $active = $(slideshow).find('IMG.active');
+
+    if ($active.length == 0)
+        $active = $(slideshow).find('IMG:last');
+
+    var $next = $active.next('IMG').length ? $active.next('IMG')
+        : $(slideshow).find('IMG:first');
+
+    $active.addClass('last-active');
+
+    $next.css({ opacity: 0.0 })
+        .addClass('active')
+        .animate(
+            { opacity: 1.0 },
+            1200,
+            function () {
+                $active.removeClass('active last-active');
+            }
+        );
+}
+
+function resizeSlideshowToImgSize(slideshow) {
+
+    let maxHeight = 0;
+
+    // We must first attach the onload event
+    // and then set the mig src so when the
+    // image is loaded, we're 100% sure the
+    // event will fire.
+    $(slideshow).find('img').each(function () {
+        this.onload = function () {
+
+            let heightThisImage = this.height;
+
+            if (heightThisImage > maxHeight) {
+                maxHeight = heightThisImage;
+
+                $(slideshow).height(maxHeight);
+            }
+        }
+        this.src = $(this).data('src');
+    });
+}
+
+function setupSlideshow() {
+    $('.pleb_buynow_item_slideshow').each(function () {
+        resizeSlideshowToImgSize(this);
+
+        if ($(this).data('disabled-slideshow') === 0) {
+            setInterval(startSlideShows, $(this).data('slideshow-transitions'), this);
+        }
+    });
+}
+
+function satsToBTC(sats) {
+    return sats / 100000000;
+}
+function satsToFiat(sats) {
+    // USD for now
+    return (satsToBTC(sats) * btcPriceInUSD).toFixed(2);
+}
+
+$(document).ready(function () {
+    setupSlideshow();
+});
--- plugin/public/js/plebeian-market-public.js
+++ plugin/public/js/plebeian-market-public.js
@@ -1,50 +1,5 @@
 let loadingModal, gpModal;
 
-function startSlideShows(slideshow) {
-    var $active = $(slideshow).find('IMG.active');
-
-    if ($active.length == 0)
-        $active = $(slideshow).find('IMG:last');
-
-    var $next = $active.next('IMG').length ? $active.next('IMG')
-        : $(slideshow).find('IMG:first');
-
-    $active.addClass('last-active');
-
-    $next.css({ opacity: 0.0 })
-        .addClass('active')
-        .animate(
-            { opacity: 1.0 },
-            1200,
-            function () {
-                $active.removeClass('active last-active');
-            }
-        );
-}
-
-function resizeSlideshowToImgSize(slideshow) {
-
-    let maxHeight = 0;
-
-    // We must first attach the onload event
-    // and then set the mig src so when the
-    // image is loaded, we're 100% sure the
-    // event will fire.
-    $(slideshow).find('img').each(function () {
-        this.onload = function () {
-
-            let heightThisImage = this.height;
-
-            if (heightThisImage > maxHeight) {
-                maxHeight = heightThisImage;
-
-                $(slideshow).height(maxHeight);
-            }
-        }
-        this.src = $(this).data('src');
-    });
-}
-
 function showLoadingModal() {
     loadingModal.show();
 }
@@ -115,15 +70,6 @@ function putIntoHtmlElementTextQrLnAddress(elementSelector, text, lnurl, qr, pro
 }
 
 $(document).ready(function () {
-    // Setup slideshows
-    $('.pleb_buynow_item_slideshow').each(function () {
-        resizeSlideshowToImgSize(this);
-
-        if ($(this).data('disabled-slideshow') === 0) {
-            setInterval(startSlideShows, $(this).data('slideshow-transitions'), this);
-        }
-    });
-
     $('.btn-buynow').click(function () {
         if (buynow_product_buying_key !== $(this).data('key')) {
             buynow_product_buying_key = $(this).data('key');
