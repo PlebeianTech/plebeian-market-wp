c8ccb4adc47c65834c26858be83c4bb570e4fe5c
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2022-11-17 10:51:33 +0100

    Cleanup

--- plugin/admin/js/plebeian-market-admin-screen-buynow.js
+++ plugin/admin/js/plebeian-market-admin-screen-buynow.js
@@ -73,14 +73,9 @@ $(document).ready(function () {
                     let isMine = row.is_mine;
 
                     let iconsToBeDisplayed = '';
-
-                    if (true) {
-                        iconsToBeDisplayed += '<img src="' + pluginBasePath + 'img/pencil-square.svg" class="dataTablesActionIcon editButton" data-pmtype="buynow" data-key="' + key + '" data-title="' + title + '" alt="Edit item" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Edit">' +
-                            '<img src="' + pluginBasePath + 'img/trash.svg" class="dataTablesActionIcon deleteButton" data-pmtype="buynow" data-key="' + key + '" data-title="' + title + '" alt="Delete item" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Delete">';
-                    }
-
+                    iconsToBeDisplayed += '<img src="' + pluginBasePath + 'img/pencil-square.svg" class="dataTablesActionIcon editButton" data-pmtype="buynow" data-key="' + key + '" data-title="' + title + '" alt="Edit item" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Edit">';
+                    iconsToBeDisplayed += '<img src="' + pluginBasePath + 'img/trash.svg" class="dataTablesActionIcon deleteButton" data-pmtype="buynow" data-key="' + key + '" data-title="' + title + '" alt="Delete item" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Delete">';
                     iconsToBeDisplayed += '<img src="' + pluginBasePath + 'img/code-square.svg" class="dataTablesActionIcon copyShortCodeButton" data-pmtype="buynow" data-key="' + key + '" alt="Copy Shortcode" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Copy shortcode">';
-
                     return iconsToBeDisplayed;
                 },
                 className: "dt-center"
