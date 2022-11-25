ddccab599ded82c914c85f17eb713b03da1444ba
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2022-11-25 15:43:22 +0100

    Removing unused code

--- common/js/plebeian-market.js
+++ common/js/plebeian-market.js
@@ -91,16 +91,6 @@ function putIntoHtmlElementTextQrLnAddress(elementSelector, text, lnurl, qr, pro
         $(elementSelector + ' .modal-title').html('');
     }
 
-    let aaa = '<div class="position-relative m-4">' +
-        '<div class="progress" style="height: 1px;">' +
-        '  <div class="progress-bar" role="progressbar" aria-label="Progress" style="width: 50%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>' +
-        '</div>' +
-        '<button type="button" class="position-absolute top-0 start-0 translate-middle btn btn-sm btn-primary rounded-pill" style="width: 2rem; height:2rem;">1</button>' +
-        '<button type="button" class="position-absolute top-0 start-50 translate-middle btn btn-sm btn-primary rounded-pill" style="width: 2rem; height:2rem;">2</button>' +
-        '<button type="button" class="position-absolute top-0 start-100 translate-middle btn btn-sm btn-secondary rounded-pill" style="width: 2rem; height:2rem;">3</button>' +
-        '</div>';
-    //$(elementSelector + ' .modal-header').html(aaa);
-
     $('#qrcodeImage').replaceWith($('<div/>').append(qr).find('svg:first').attr('id', 'qrcodeImage'));
 }
 
